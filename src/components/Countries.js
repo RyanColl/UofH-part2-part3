b0b2ce2a6085177bc0ad8cf378dfd2caf53c50cf
Ryan <ryancollicutt22@gmail.com>
2021-07-05 17:44:45 -0700

    challenging

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,22 +1,40 @@
+import { useState } from "react"
+import Country from "./Country"
 
 
 const Countries = (props) => {
-    let k = []
+    
+    // store the filtered array objects in this to get a length. This changes dynamically with the app. so cool!
+    let filteredPropsArray = props.data.filter((country, i) =>  country.name.indexOf(props.filter) !== -1)
+    if(filteredPropsArray.length <= 10 && filteredPropsArray.length !== 1) {
         return(
           <div>
               <h3>Countries</h3>
               <ul>
                 { 
                     props.data.map((country, i) => {
-                    console.log(country.name.indexOf(props.filter))
                     if(country.name.indexOf(props.filter) > -1) return(
-                        <li key={i}>
-                            {country.name}
-                        </li>
+                        <Country key={i} name={country.name} filter={props.filter} />
                     )
                 })}
              </ul>
           </div>
         )
+    } else if(filteredPropsArray.length === 1) return(
+        <Country key={1} data={filteredPropsArray[0]}/>
+    )
+    else if(filteredPropsArray.length === 250) return (
+        <div>
+                <h3>Countries</h3>
+                <p>Please type something</p>
+        </div>
+    ) 
+    else return (
+            <div>
+                <h3>Countries</h3>
+                <p>Please be more specific</p>
+            </div>
+        )
+   
 }
 export default Countries
---
+++ src/components/Country.js
@@ -0,0 +1,27 @@
+
+const Country = (props) => {
+    if(props.data) return(
+        <div>{console.log(props.data)}
+            <h1>{props.data.name}</h1>
+            <p>Capital: {props.data.capital} </p>
+            <p>Population: {props.data.population}</p>
+            <h3>Languages</h3>
+            <ul>
+                {props.data.languages.map((language, i) => {
+                    return(
+                        <li key={i}>
+                            {language.name}
+                        </li>
+                    )
+                    })}
+            </ul>
+            <img src={props.data.flag} width='150' height='150'  />
+        </div>
+    )   
+    else return (
+        <li>
+            {props.name}
+        </li>
+    )
+}
+export default Country
